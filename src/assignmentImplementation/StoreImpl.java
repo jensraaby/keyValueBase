39e00908562b423aaaa74f6693bd7be977afb15d
Jens Raaby <jens+github@m.raaby.co.uk>
2012-11-27 21:47:09 +0100

    This is a basic implementation and test of the Store implementation. I include a simple test file to demonstrate.

--- src/assignmentImplementation/StoreImpl.java
+++ src/assignmentImplementation/StoreImpl.java
@@ -1,19 +1,96 @@
 package assignmentImplementation;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+
+import keyValueBaseExceptions.ServiceInitializingException;
+import keyValueBaseInterfaces.MemoryMappedFile;
 import keyValueBaseInterfaces.Store;
 
 public class StoreImpl implements Store
 {
+	/**
+	 * @author Jens Raaby
+	 * Store the memory mapped file privately
+	 */
+	private MemoryMappedFile memory;
+	private RandomAccessFile raf;
+	/**
+	 * 
+	 * @param filePath
+	 * The path to a file for storage
+	 * @param diskSize
+	 * An initial file size (effectively it reserves space on the disk)
+	 * @param totalSize
+	 * Maximum addressable space. When the file is full, it can increase in size. 
+	 * But I think an exception should be thrown if the initial size is > the total  
+	 */
+	public StoreImpl(String filePath, long totalSize) {
+		
+		
+		try {
+			
+			// Based partially on http://www.javamex.com/tutorials/io/nio_mapped_buffer.shtml
+			// Create a file to hold data on disk
+			raf = new RandomAccessFile(filePath, "rw");
+			// Preallocate Set file size (bytes)
+			raf.setLength(totalSize); // e.g size = 2048, size will be 2.1 MB
+			FileChannel fc = raf.getChannel();
+			System.out.println("File size: " + fc.size());
+			
+			//n.b. buffer sets the initial position - 0 should be an OK start
+			memory = new MemoryMappedFile(fc, FileChannel.MapMode.READ_WRITE, 0, totalSize);
+		}
+		catch (IOException ex) 
+		{
+			//TODO: handle exceptions
+			ex.printStackTrace();
+		}
+		catch (IndexOutOfBoundsException ex) 
+		{
+			//TODO: handle exceptions
+			ex.printStackTrace();
+		}
+		
+		
+	}
+	
+	//TODO: remove this method!
+	public void closeFile() 
+	{
+		try {
+			raf.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
+	/**
+	 * Read takes a position
+	 * 
+	 * @param position
+	 * The starting position in the address space
+	 * 
+	 * @param length
+	 * The byte length - assumed to fit within the memory file (the calling method should check this)
+	 */
 	@Override
 	public byte[] read(Long position, int length) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		byte[] block = new byte[length];
+		System.out.println("read byte length: " + block.length);
+		
+		memory.get(block, position);
+
+		return block;
 	}
 
 	@Override
 	public void write(Long position, byte[] value) {
-		// TODO Auto-generated method stub
+		memory.put(value, position);
 		
 	}
 	
---
+++ src/testing/storeTest.java
@@ -0,0 +1,50 @@
+package testing;
+
+import java.io.File;
+
+import assignmentImplementation.StoreImpl;
+
+public class storeTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			String path = "tempfile";
+		
+			/**
+			 *  This just creates a store (in project dir), then writes some bytes
+			 *  Then we check that the read bytes match up
+			 */
+			StoreImpl store = new StoreImpl(path, 2048);
+			
+			
+			byte[] testwrite = {-100,10,1,2,3};
+			for (int i = 0; i< testwrite.length; i++) {
+				System.out.print(testwrite[i] + ", ");
+				
+			}
+			System.out.println();
+			System.out.println("created store, writing bytes");
+	
+			store.write((long) 0,testwrite);
+	
+			byte[] testread = store.read((long) 0, testwrite.length);
+			
+			System.out.println("read: length: " + testread.length);
+			for (int i = 0; i< testread.length; i++) {
+				System.out.print(testread[i] + ", ");
+			}
+			store.closeFile();
+			File f = new File(path);
+			f.delete();
+	
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
